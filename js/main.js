0dd4e9b5cbfb88374fbd2277e95ecb7de1afb71b
woody <45289944+woody21978@users.noreply.github.com>
2020-05-28 18:26:16 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -9,4 +9,13 @@ $(document).ready(function(){
     $('.block-about-table__list_next').fadeIn();
     $('#nextContent').fadeOut();
   });
+  $('.menu-burger').click(function() {
+    this.classList.toggle('menu-burger__active');
+    let menu_block = document.querySelector('.menu-burger-list');
+    if (this.classList.contains('menu-burger__active')) {
+      menu_block.classList.add('popup__active');
+    } else {
+      menu_block.classList.remove('popup__active');
+    }
+  });
 });
